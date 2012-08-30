6290ba73f67d91ee48358a642c5c86f32538041f
developej <developej@gmail.com>
2012-08-30 21:57:58 +0200

    new problems

---
+++ problem7/src/java/org/problem7.java
@@ -0,0 +1,23 @@
+public class problem7 {
+	static int counter = 1;
+
+	public static void main(String[] args) {
+		for (int i = 3; ; i++) {
+			if (isPrime(i)) {
+				counter += 1;
+				if (counter == 10001) {
+					System.out.println(i);
+				}
+			}
+		}
+	}
+
+	public static boolean isPrime(int p) {
+		for (int i = 2; i < p; i++) {
+			if (p % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
